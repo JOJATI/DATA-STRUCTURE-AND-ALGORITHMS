748fceb1a423634e13d1f37931ca8937693390eb
J BHAKAT <jojati2005@gmail.com>
2026-07-08 22:50:38 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (43.06%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0 || n<0){
+        if(n==0){
+            return false;
+            
+        }
+        else if(n<0)
+        {
             return false;
         }
         else
