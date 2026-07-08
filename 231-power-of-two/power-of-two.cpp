b565b6eb62397d4be05f7cf2e33a4e60793c3d0c
J BHAKAT <jojati2005@gmail.com>
2026-07-08 22:18:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (11.98%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -4,7 +4,7 @@ public:
         for(int i=0;i<=30;i++){
             if(n==pow(2,i)){
                 return true;
-                break;
+             
             }
         }
         return false;
