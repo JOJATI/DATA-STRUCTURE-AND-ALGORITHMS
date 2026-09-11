c77b6bdc4bbb7e05b761b0705dec10a11e3c6578
J BHAKAT <jojati2005@gmail.com>
2026-09-11 14:44:44 +0530

    Time: 6 ms (36.43%) | Memory: 39.7 MB (11.96%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.cpp
+++ 2722-prime-in-diagonal/prime-in-diagonal.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    int prime(int x)
-    {   int flag=0;
-    if(x<2) return 0;
-        for(int i=2;i*i<=x;i++)
-        {
-            if(x%i==0)
-            {   flag=1;
-                return 0;
-            }
-        }
-        return 1;
+   bool prime(int n){
+    if(n<2) return false;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return false;
     }
+        return true;
+   }
     int diagonalPrime(vector<vector<int>>& nums) {
         int max_elem=0;
         for(int i=0;i<nums.size();i++){
