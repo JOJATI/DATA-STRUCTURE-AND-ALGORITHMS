3069627ce2ae32a672df75517890f6f8070b98a9
J BHAKAT <jojati2005@gmail.com>
2026-09-13 23:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.83%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        while(n>=1){
+            if((n&1)==((n>>1)&1))
+                return false;
+            n=n>>1;
+            }
+            
+
+            
+            return true;
+        
+        
+    }
+};
