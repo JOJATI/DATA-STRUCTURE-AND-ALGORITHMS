20822d366fcfeb4b2844d5d92b2715a69d7d9845
J BHAKAT <jojati2005@gmail.com>
2026-09-22 22:56:17 +0530

    Time: 943 ms (5.16%) | Memory: 248.9 MB (5.64%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,26 +1,31 @@
-const int n = 5*1e6 + 1;
-vector<bool>isPrime(n+1, true);
-    
 class Solution {
 public:
-    
-    void Compute(){
+    int countPrimes(int n) {
+
+        if (n <= 2)
+            return 0;
+
+        vector<char> isPrime(n, true);
+
         isPrime[0] = isPrime[1] = false;
-        if(isPrime[4] == false)return;
-        for (int i = 2; (long long)i * i <= n; i++) {
+
+        for (int i = 2; i * i < n; i++) {
+
             if (isPrime[i]) {
-                for (int j = i * i; j <= n; j += i)
+
+                for (int j = i * i; j < n; j += i) {
                     isPrime[j] = false;
+                }
             }
         }
-    }
-    int countPrimes(int n) {
-        Compute();
-        int cnt=0;
-       
-        for(int i = 2 ; i < n ; i++){
-            if(isPrime[i])cnt++;
+
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i])
+                count++;
         }
-        return cnt;
+
+        return count;
     }
 };
